561b83ef545f38ffa00d46bea9afbd4656627098
Leanne Dong <jdleanne@gmail.com>
2021-02-03 01:30:07 -0500

    bug fixed. Full insel model.txt parsed.

--- main.cpp
+++ main.cpp
@@ -8,20 +8,58 @@ using std::cerr;
 using std::string;
 
 int main() {
-    int num_lines = 0;
     std::ifstream file( "pila.txt");
     std::string line;
 
-       std::ofstream output("output.txt");
+    std::ofstream output("output.txt");
     while(getline(file, line))
     {
-        //if(line.find("S") != std::string::npos)
-        if (line[0]=='S')
-        {
-            ++num_lines;
-            std::cout << " The lines start with letter S are " << line << "\n";
-            output << line << "\n";
+        std::istringstream line_stream(line);
+        string word;
+        line_stream >> word;
+        if (word == "S") {
+            // Start of a block;
+            int block_number;
+            line_stream >> block_number;
+
+            if (!line_stream) {
+                assert(false); // not implemented
+            }
+
+            string block_type;
+            line_stream >> block_type;
+
+            cerr << word << " " << block_number << " " << block_type << "\n";
+
+            if ((block_type == "CURRENTEDGE")||(block_type == "VERTEX")) {
+                getline(file, line);
+                cerr << line << "\n";
+            }
         }
+        else if (word == "P") {
+            int block_number;
+            line_stream >> block_number;
+            cerr << word << " " << block_number << "\n";
+            if (!line_stream) {
+                assert(false); // not implemented
+            }
+
+            //cerr << "Beginning parameters for block " << block_number << "\n";
+            getline(file, line);
+            cerr << line << "\n";
+        }
+        else {
+            cerr << "Unknown line: " << line << "\n";
+            assert(false); // not implemented
+        }
+
+//        if (line[0]=='S')
+//        {
+//            ++num_lines;
+//            std::cout << " The lines start with letter S are " << line << "\n";
+//            output << line << "\n";
+//        }
+
     }
-    std::cout << "Number of lines contain S: " << num_lines;
+   // std::cout << "Number of lines contain S: " << num_lines;
 }
